d6f442cdf1da7092727eb61464cde24279f4c39c
rahulg009 <63463686+rahulg009@users.noreply.github.com>
2020-05-21 14:58:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.post("/blogs", function(req, res){
     // create blog
     console.log(req.body);
 	req.body.blog.body = req.sanitize(req.body.blog.body)
+    console.log("===========")
     console.log(req.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
